035628075e706e7c90c8d541f8564e9ef19e15af
ChanHo <hpyho33@kookmin.ac.kr>
2019-11-06 18:28:35 +0900

    firebase set

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 
 const app = firebase.initializeApp({
-  apiKey: 'AIzaSyBzWWLpdhfmUV0f6wLjQEM9qg825XKTkmo',
-  authDomain: 'kmu-osp-gg.firebaseapp.com',
-  databaseURL: 'https://kmu-osp-gg.firebaseio.com',
-  projectId: 'kmu-osp-gg',
-  storageBucket: 'kmu-osp-gg.appspot.com',
+  apiKey: 'AIzaSyACjXUSx9jOMLOGJHiw5Hx51gTI4lsitlc',
+  authDomain: 'https://wink-pb-project.firebaseapp.com/',
+  databaseURL: 'https://wink-pb-project.firebaseio.com',
+  projectId: 'wink-pb-project',
+  storageBucket: 'wink-pb-project.appspot.com',
   messagingSenderId: '50247262024',
   appId: '1:50247262024:web:ac66e03d555c3b0f5d5d7d',
 });
